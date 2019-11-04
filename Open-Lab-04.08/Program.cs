d705c12cf9046e4d728f1b12824d61d936434d25
romankalab <roan.kalab@gmail.com>
2019-11-04 10:03:25 +0000

    Initial

---
+++ Open-Lab-04.08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Open_Lab_04._08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringarr = new string[4];
+            for (var x = 0; x < stringarr.Length; x++)
+                stringarr[x] = Console.ReadLine();
+                
+
+            Words words = new Words();
+            string[] arr1 = words.FourChar(stringarr);
+            foreach (var item in arr1)
+                Console.Write(item + ", ");
+        }
+    }
+}
---
+++ Open-Lab-04.08/Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_04._08
+{
+    class Words
+    {
+        public string[] FourChar(string[] stringarr)
+        {
+            var lis = new List<string> { };
+            foreach (var item in stringarr)
+                if (item.ToCharArray().Length == 4)
+                    lis.Add(item);
+            return lis.ToArray();
+
+        }
+    }
+}
